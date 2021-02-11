77e8cde27ade5054cc904a4d6b09105395e714cb
niaanderson <niaanderson@nias-mbp.fios-router.home>
2021-02-11 17:45:16 -0500

    Assignment 2 Work

--- large-scale-programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ large-scale-programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,8 +1,20 @@
 // Nia Anderson LSP HW 1
 package org.howard.edu.lsp.assignment1;
-
+/**
+ * This class is my hello world implementation that contais my program
+ * @author niaanderson
+ *Comment your code for every method that you write
+ */
 public class HelloWorld {
-
+	/**
+	 * This method .....
+	 * @param i i is a counter
+	 * @return a negative value
+	 */
+	public static int foo(int i) {
+		return -1;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 
