e84f2b7db2ec53a0ecec44f08dbd0566372784d8
niaanderson <niaanderson@nias-mbp.fios-router.home>
2021-04-23 12:00:40 -0400

    TestTollBooth

--- large-scale-programming/src/org/howard/edu/assignment7/junit/TestTollBooth.java
+++ large-scale-programming/src/org/howard/edu/assignment7/junit/TestTollBooth.java
@@ -5,19 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TestTollBooth {
-public static void main(String [] args)
-   {
-Tolling booth = new Tolling();
-truckOne Toyota = new truckOne(5,12000);
-truckOne Tesla = new truckOne(2,5000);
-truckOne Chevy = new truckOne(6,17000);
-booth.tollCalculator(Toyota);
-booth.dataDisplayed();
+	public static void main(String [] args){
+		Tolling booth = new Tolling();
+		truckOne Toyota = new truckOne(5,12000);
+		truckOne Tesla = new truckOne(2,5000);
+		truckOne Chevy = new truckOne(6,17000);
+		booth.tollCalculator(Toyota);
+		booth.dataDisplayed();
 
-booth.tollCalculator(Tesla);
-booth.dataDisplayed();
+		booth.tollCalculator(Tesla);
+		booth.dataDisplayed();
 
-booth.tollCalculator(Chevy);
-booth.dataDisplayed();
-}
+		booth.tollCalculator(Chevy);
+		booth.dataDisplayed();
+	}
 }
